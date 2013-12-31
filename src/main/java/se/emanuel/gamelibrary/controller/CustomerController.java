358716b11582488f61aa0d0daf1997de6dfd23e3
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    added register for new user

--- src/main/java/se/emanuel/gamelibrary/controller/CustomerController.java
+++ src/main/java/se/emanuel/gamelibrary/controller/CustomerController.java
@@ -1,7 +1,30 @@
 package se.emanuel.gamelibrary.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import se.emanuel.gamelibrary.service.CustomerService;
 
 @Controller
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+    @GetMapping("create")
+    public String getLoginCreate() {
+        return "redirect:/registerpage.html";
+    }
+    @PostMapping("createlogin")
+    public String addUser(Model model,
+                           @RequestParam String name,
+                           @RequestParam String lastname,
+                           @RequestParam String address,
+                           @RequestParam String username,
+                           @RequestParam String password,
+                           @RequestParam int age) {
+        model.addAttribute("admin",customerService.createLogin(name, lastname, address, username, password, age));
+        return "redirect:/loginpage.html";
+    }
 }
--- src/main/java/se/emanuel/gamelibrary/service/CustomerService.java
+++ src/main/java/se/emanuel/gamelibrary/service/CustomerService.java
@@ -1,4 +1,32 @@
 package se.emanuel.gamelibrary.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.annotation.SessionScope;
+import se.emanuel.gamelibrary.entity.Customer;
+import se.emanuel.gamelibrary.repository.CustomerRepo;
+
+@Service
+@SessionScope
 public class CustomerService {
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Transactional
+    public String createLogin(String name, String lastname, String address, String username, String password, int age) {
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setLastname(lastname);
+        customer.setAddress(address);
+        customer.setUsername(username);
+        customer.setPassword(password);
+        customer.setAge(age);
+        customer.setRole(0);
+
+        if (!customerRepo.existsByUsername(username) && age > 15) {
+            customerRepo.save(customer);
+            return "Successfull saved";
+        } else return "already exists";
+    }
 }
